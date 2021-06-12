435478eed2eb56dcb48378739ae492d2d36df103
onepies <85448071+onepies@users.noreply.github.com>
2021-06-12 22:54:06 +0700

    Update king.py

--- userbot/modules/king.py
+++ userbot/modules/king.py
@@ -47,34 +47,34 @@ async def typewriter(typew):
 # Create by myself @localheart
 
 
-@register(outgoing=True, pattern="^.king(?: |$)(.*)")
+@register(outgoing=True, pattern="^.kyud(?: |$)(.*)")
 async def _(event):
     event.pattern_match.group(1)
     await event.edit(
         " -^-^-^- **K** -^-^-^- \n"
-        " -^-^-^- **I** -^-^-^- \n"
-        " -^-^-^- **N** -^-^-^- \n"
-        " -^-^-^- **G** -^-^-^- \n"
+        " -^-^-^- **Y** -^-^-^- \n"
+        " -^-^-^- **U** -^-^-^- \n"
+        " -^-^-^- **D** -^-^-^- \n"
         "__**USERBOT TELEGRAM**__ \n"
         "━━━━━━━━━━━━━━━━━━━━━\n"
-        f"__**Hai aku adalah bot king assisten {ALIVE_NAME} yang menjaga akun ini**__ \n"
+        f"__**Hai aku adalah bot kyud assisten {ALIVE_NAME} yang menjaga akun ini**__ \n"
         "__**Terimakasih**__ \n"
         "`Enjoy:)` \n"
-        "__**Developer by :**__ [Apis](https://t.me/PacarFerdilla) ")
+        "__**Owner by :**__ [Kyud](https://t.me/boyfriendnice) ")
 
 CMD_HELP.update(
     {
-        "king": "**✘ Plugin :** `king`\
-        \n\n  •  **Perintah :** `.king`\
+        "king": "**✘ Plugin :** `kyud`\
+        \n\n  •  **Perintah :** `.kyud`\
         \n  •  **Function : **Untuk melihat sesuatu yang menarik\
         \n\n  •  **Perintah :** `.sadboy`\
         \n  •  **Function : **Jadi sadboy:)\
         \n\n  •  **Perintah :** `.punten` | `.pantau`\
         \n  •  **Function : **Untuk punten dan pantau\
         \n\n  ** Perintah kosong **\
-        \n  ** Harap chat developer king @PacarFerdilla Jika ingin mengidekan sesuatu yang menarik **\
+        \n  ** Harap chat owner kyud @boyfriendnice Jika ingin mengidekan sesuatu yang menarik **\
         \n\n  ** Perintah kosong **\
-        \n  ** Harap chat developer king @PacarFerdilla Jika ingin mengidekan sesuatu yang menarik **\
+        \n  ** Harap chat owner kyud @boyfriendnice Jika ingin mengidekan sesuatu yang menarik **\
     "
     }
 )
